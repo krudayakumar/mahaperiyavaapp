41b2818f2e20d2cadec1a2acadfe557754042d23
m84098 <udayakumar.k@cigna.com>
2015-10-11 03:03:43 -0400

    Changes in commit

---
+++ app/src/main/assets/json/test.py
@@ -0,0 +1,25 @@
+import httplib
+
+def printText(txt):
+    lines = txt.split('\n')
+    for line in lines:
+        print line.strip()
+
+httpServ = httplib.HTTPConnection("127.0.0.1", 80)
+httpServ.connect()
+
+httpServ.request('GET', "/test.html")
+
+response = httpServ.getresponse()
+if response.status == httplib.OK:
+    print "Output from HTML request"
+    printText (response.read())
+
+httpServ.request('GET', '/cgi_form.cgi?name=Brad&quote=Testing.')
+
+response = httpServ.getresponse()
+if response.status == httplib.OK:
+    print "Output from CGI request"
+    printText (response.read())
+
+httpServ.close()
